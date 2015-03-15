13e1c6687443155570eca306125dabbf566f1292
Judson Mitchell <judsonmitchell@gmail.com>
2015-03-15 06:49:12 -0500

    Complete JSON encode

--- gen_la_holidays.py
+++ gen_la_holidays.py
@@ -1,11 +1,21 @@
-from datetime import date
+from time import mktime
 from workalendar.usa import Louisiana
+import datetime
 import json
+
 cal = Louisiana()
-d = cal.holidays(2015)
-output = []
-for item in d:
-    output.append(str(item[0]))
+d = cal.holidays(2030)
+
+#Class shamelessly ripped off from http://stackoverflow.com/a/10721564/49359
+class EncodeDate(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            #return int(mktime(obj.timetuple()))
+            return obj.strftime ('%Y-%m-%d')
+
+        return json.JSONEncoder.default(self, obj)
+
 
 #print output
-print json.dumps(output)
+print json.dumps(d, cls=EncodeDate)
